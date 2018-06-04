52fe95ef7c246331c3107ff28be0f231aed0843f
Hannah Hughes <hugheshannah96@gmail.com>
2018-06-04 17:01:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ function takeANumber(line, name) {
   line.push(name)
 
   return `Welcome, ${name}. You are number ${line.length} in line.`
-};
+}
+
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  }
+  
+  return 'Currently serving ${line.shift()}.'
+}
