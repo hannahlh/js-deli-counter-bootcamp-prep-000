0567db81ea0d2f1f7af91483dbf842411c3d851a
Hannah Hughes <hugheshannah96@gmail.com>
2018-06-04 17:05:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ function currentLine(line) {
     numbersAndNames.push(`${i + 1}. ${line[i]}`)
   }
   
-  return 'The line is currently: ${numbersAndNames.join(', ')}`
+  return `The line is currently: ${numbersAndNames.join(', ')}`
 }
